8ed7e594519677d7664d988bd93175b1392523b0
Samuel Mbah <samuelcmbah@gmail.com>
2026-04-15 11:18:41 +0100

    allow api to receive data, store it permanently in turso and read it back

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,12 +1,23 @@
 import { Hono } from 'hono'
+import { createClient } from '@libsql/client'
 
 
 // defines the expected shape of the environment variables
 type Bindings = {
 	GROQ_API_KEY: string
+	TURSO_DATABASE_URL: string
+  TURSO_AUTH_TOKEN: string
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
+
+//helper function to connect to the Turso database
+const getDbClient = (env: Bindings) => {
+	return createClient({
+		url: env.TURSO_DATABASE_URL,
+		authToken: env.TURSO_AUTH_TOKEN
+	})
+}
 
 app.get('/', (contextObject) => {
 	return contextObject.text('RoleSnap API is running!')
@@ -64,4 +75,67 @@ app.post('/api/parse', async (contextObj) => {
 		return contextObj.json({ error: 'Failed to parse job post' }, 500)
 	}
 		})
+
+// Save job endpoint
+app.post('/api/jobs', async (contextObj) => {
+	const db = getDbClient(contextObj.env)
+
+	const jobData = await contextObj.req.json()
+
+	const tempUserId = 'user-123' // get from Clerk later in phase 2
+	const jobId = crypto.randomUUID()
+
+	try {
+		await db.execute({
+			sql: `INSERT INTO jobs (
+						id, user_id, title, company, location, salary, stack,
+					  contact_info, source_url, raw_text, status, source_method
+			) 
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+			args: [
+        jobId,
+        tempUserId,
+        jobData.title || 'N/A',
+        jobData.company || 'N/A',
+        jobData.location || 'N/A',
+        jobData.salary || 'N/A',
+        JSON.stringify(jobData.stack || []), // Store array as JSON string
+        jobData.contact_info || 'N/A',
+        jobData.source_url || 'N/A',
+        jobData.raw_text || '',
+        'saved',
+        jobData.source_method || 'manual'
+      ]
+		})
+		
+		return contextObj.json({ success: true, id: jobId }, 201)
+	} catch (error) {
+		console.error('Error saving job:', error)
+		return contextObj.json({ error: 'Failed to save job' }, 500)
+	}
+})
+
+// List Jobs Route ---
+app.get('/api/jobs', async (contextObj) => {
+  const db = getDbClient(contextObj.env)
+  const tempUserId = "user-123"
+
+  try {
+    const result = await db.execute({
+      sql: "SELECT * FROM jobs WHERE user_id = ? ORDER BY created_at DESC",
+      args: [tempUserId]
+    })
+    
+    // Parse the stack string back into an array for the frontend
+    const jobs = result.rows.map(row => ({
+      ...row,
+      stack: JSON.parse(row.stack as string || '[]')
+    }))
+
+    return contextObj.json(jobs)
+  } catch (e) {
+    return contextObj.json({ error: 'Failed to fetch jobs' }, 500)
+  }
+})
+
 export default app
